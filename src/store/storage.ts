26057ba1313715a997c5b3b91fea30c194b3ddf5
ahmedyahya12 <ahmed4yahya4@gmail.com>
2026-05-02 14:48:08 +0000

    fix redux persist storage for vercel

--- src/store/storage.ts
+++ src/store/storage.ts
@@ -1,23 +1,23 @@
+// src/store/storage.ts
 import createWebStorage from 'redux-persist/lib/storage/createWebStorage';
-import type { WebStorage } from 'redux-persist/es/types';
 
-const createNoopStorage = (): WebStorage => {
+const createNoopStorage = () => {
   return {
     getItem(_key: string): Promise<string | null> {
       return Promise.resolve(null);
     },
-
     setItem(_key: string, _value: string): Promise<void> {
       return Promise.resolve();
     },
-
     removeItem(_key: string): Promise<void> {
       return Promise.resolve();
     },
   };
 };
 
-const storage: WebStorage =
-  typeof window !== 'undefined' ? createWebStorage('local') : createNoopStorage();
+const storage =
+  typeof window !== 'undefined'
+    ? createWebStorage('local')
+    : createNoopStorage();
 
 export default storage;
